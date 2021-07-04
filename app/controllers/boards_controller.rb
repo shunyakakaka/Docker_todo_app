5780b312ee245239caedad2bacf22889ef108217
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2021-07-04 16:47:48 +0900

    newフォームの制作

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,27 @@
 class BoardsController < ApplicationController
   def index
+    @boards = Board.all
   end
 
   def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    @board.save
+    flash[:notice] = "#{@board.title}の掲示板を作りました"
+    redirect_to boards_path
   end
 
   def edit
   end
 
   def show
+  end
+  private
+
+  def board_params
+    params.require(:board).permit(:name, :title, :body)
   end
 end
